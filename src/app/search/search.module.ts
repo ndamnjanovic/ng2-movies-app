ebb9773172def5240be3c1f057761c45d92e74bc
Nedeljko Damnjanovic <nedeljko.damnjanovic@gmail.com>
2017-09-02 17:22:33 +0200

    Add validation for search component

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { SearchComponent } from './components/search/search.component';
@@ -7,7 +8,11 @@ import { SearchComponent } from './components/search/search.component';
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    FormsModule
+  ],
+  exports: [
+    SearchComponent
   ],
   declarations: [
     SearchComponent
